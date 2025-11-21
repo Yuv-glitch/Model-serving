fa212dabb97b012367ff84d4c91f87d45f121210
Yuv-glitch <68955738+Yuv-glitch@users.noreply.github.com>
2025-11-21 10:54:53 +0530

    Delete inpt.py

--- inpt.py
+++
@@ -1,7 +0,0 @@
-from classifier import InjectionClassifier
-cls = InjectionClassifier()
-
-def is_malicious_input(text: str) -> bool:
-    if cls.is_malicious(text):
-        return True
-    return False
